bae4f6685838e448235cab6fbcfef661359564c2
suryakumarsirvi <suryakumarsirvi@gmail.com>
2026-02-20 02:39:07 +0530

    refactor: restructure code

--- algo.js
+++ algo.js
@@ -3,3 +3,8 @@ function toCamel(str) {
   return str.replace(/[-_](.)/g, (_, c) => c.toUpperCase());
 }
 
+// Check empty object
+function isEmpty(obj) {
+  return Object.keys(obj).length === 0;
+}
+
