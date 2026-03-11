e600f6c1034cc79a4feee73ec832a8f340254c72
suryakumarsirvi <suryakumarsirvi@gmail.com>
2026-03-11 07:25:57 +0530

    feat(auth): clerk add and login page ui done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { Route, Routes } from "react-router";
+import Login from "../src/pages/Auth/Login";
+import Feed from "../src/pages/Feed";
+import Messages from "../src/pages/Messages";
+import ChatBox from "../src/pages/ChatBox";
+import Connections from "../src/pages/Connections";
+import Discover from "../src/pages/Discover";
+import Profile from "../src/pages/Profile";
+import CreatePost from "../src/pages/CreatePost";
+import { useUser } from "@clerk/react";
+import Layout from "./layout/Layout";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const {user} = useUser();
 
-export default App
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={!user ? <Login /> : <Layout/>}>
+          <Route index element={<Feed />} />
+          <Route path="messages" element={<Messages />} />
+          <Route path="messages/:userId" element={<ChatBox />} />
+          <Route path="connections" element={<Connections />} />
+          <Route path="discover" element={<Discover />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="profile/:profileId" element={<Profile />} />
+          <Route path="create-post" element={<CreatePost />} />
+        </Route>
+      </Routes>
+    </>
+  );
+};
+
+export default App;
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,10 +1,15 @@
-
+import React from 'react'
+import Sidebar from '../components/common/Sidebar'
+import { Outlet } from 'react-router'
 
 const Layout = () => {
   return (
-    <>
-
-    </>
+    <div className='w-full flex h-screen'>
+      <Sidebar/>
+      <div className='flex-1 bg-slate-50'>
+        <Outlet />
+      </div>
+    </div>
   )
 }
 
